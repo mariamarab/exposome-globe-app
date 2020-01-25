3c56d4cb6aec8537cf8c8a65dcc543bcc61be154
arab <arab@sfu.ca>
2020-01-25 10:20:53 -0800

    Updates

---
+++ precache-manifest.3fe20025cfd27a52cc6f9c3f806b6b4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112d9bb7b62bb14c2a0c659f65e30a18",
+    "url": "/exposome-globe-app/index.html"
+  },
+  {
+    "revision": "dded9e0197ebb37d0395",
+    "url": "/exposome-globe-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "651907a7e02234f7cfcc",
+    "url": "/exposome-globe-app/static/js/2.33af256b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/exposome-globe-app/static/js/2.33af256b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dded9e0197ebb37d0395",
+    "url": "/exposome-globe-app/static/js/main.9d45ea61.chunk.js"
+  },
+  {
+    "revision": "a53999a59e87b4a98c20",
+    "url": "/exposome-globe-app/static/js/runtime-main.bb344d73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/exposome-globe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
